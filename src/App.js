10c0c13b616d3a7c929fee4654b98e6dd520970c
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-16 15:21:27 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Style from "./global.css";
 
 function App() {
+  const [count, setCount] = useState([]);
+  const thing = count.map(img => {
+    return img
+  });
+  const length = count.length;
+  const thingminus = count.map(img => {
+    return img
+  })
+  function removeLode(){
+    const size = count[length];
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Gusis OG lode sip count</h1>
+    <button  onClick={() => setCount([...count, <img src="./lode2.png" width="60px" height="160px"/>])}>Lode + count</button>
+    <button  onClick={removeLode()}>Lode - count</button>
+    <br></br>
+    <br></br>
+    {thing}
+
+    </>
   );
 }
 
